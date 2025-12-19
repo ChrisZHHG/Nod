f0ac7fdf8b552af54661afaff281fe7408a5d028
Chris Z <120712688+ChrisZHHG@users.noreply.github.com>
2025-12-19 14:42:01 -0800

    Initial Commit

---
+++ Ambrosia/AmbrosiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbrosiaApp.swift
+//  Ambrosia
+//
+//  Created by Chris on 2025-12-19.
+//
+
+import SwiftUI
+
+@main
+struct AmbrosiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmbrosiaTests/AmbrosiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  AmbrosiaTests.swift
+//  AmbrosiaTests
+//
+//  Created by Chris on 2025-12-19.
+//
+
+import Testing
+@testable import Ambrosia
+
+struct AmbrosiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
